e77e0efaa744563fbf1115a568951d74ab5fdfd3
Eridani System <osf319@gmail.com>
2026-01-29 20:56:53 -0500

    continued

--- postInit/bgbTiCon.groovy
+++ postInit/bgbTiCon.groovy
@@ -1,21 +1,96 @@
 import static postInit.GlobalItems.*
-import static postInit.TiConMaterialBuilder.*
+import static postInit.TiConMatBuilder.*
 
-new TiConMaterialBuilder('titanium', 0xC8C8FF)
-.head(600, 8f, 5f, 3) // durability, miningSpeed, attack, harvestLevel
-.handle(0.95f, 175) // modifier, durability
-.extra(50) // durability
-.bow(1.5f, 3f, 9.5) // drawSpeed, range, bonusDamage
-.addTrait('lightweight', 'head')
-.addTrait('lightweight', 'handle')
-.addTrait('established', 'extra')
-.item('libvulpes:productingot', 7)
-.fluid('titanium_molten')
-.register()
+/*
+    Titanium Stats
+    Color - 0xCCC8FA
 
-/* 
-new TiConMaterialBuilder('titaniumaluminide', 0xC8C8C8)
-.head(400, 8f, 6f, 4)
-.handle(1.05f, 200)
-.extra(100)
+    Head
+    Durability - 600
+    Mining Speed - 8
+    Attack - 5
+    Harvest Level - Obsidian (3)
+    Trait - Lightweight
+
+    Handle
+    Modifier - 0.95f
+    Durability - 175
+    Trait - Lightweight
+
+    Extra
+    Durability - 50
+    Trait - Well-Established
+
+    Bow
+    Draw Speed - 1.5
+    Range - 3
+    Bonus Damage - 9.5
+*/
+createMaterial('titanium', 0xCCC8FA) {
+    // Basic info
+    castable()
+    setFluid(fluid('titanium_molten'))
+    addItem(item('libvulpes:productingot:7'))
+
+    // Stats
+    addHeadStats(600, 8f, 5f, 3)
+    addHandleStats(0.95f, 175)
+    addExtraStats(50)
+    addBowStats(1.5f, 3f, 9.5f)
+
+    // Traits
+    addTrait('lightweight', null)
+    addTrait('established', 'extra')
+}
+
+/*
+    Titanium Aluminide Stats
+    Color - 0xAEC2DE
+
+    Head
+    Durability - 
+    Mining Speed - 
+    Attack - 
+    Harvest Level - 
+    Trait - 
+
+    Handle
+    Modifier - 
+    Durability - 
+    Trait - 
+
+    Extra
+    Durability - 
+    Trait - 
+
+    Bow
+    Draw Speed - 
+    Range -
+    Bonus Damage - 
+*/
+
+/*
+    Titanium-Iridium Alloy Stats
+    Color - 0xD7DFE4
+
+    Head
+    Durability - 
+    Mining Speed - 
+    Attack - 
+    Harvest Level - 
+    Trait - 
+
+    Handle
+    Modifier - 
+    Durability - 
+    Trait - 
+
+    Extra
+    Durability - 
+    Trait - 
+
+    Bow
+    Draw Speed - 
+    Range - 
+    Bonus Damage - 
 */
