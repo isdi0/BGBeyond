72085151fa5d1b9e1fb6676bda71ba9cafa0677e
Eridani System <osf319@gmail.com>
2026-01-30 19:03:54 -0500

    moved recipe removal code within recipes.groovy for clarity

--- postInit/recipes.groovy
+++ postInit/recipes.groovy
@@ -1,22 +1,22 @@
 import static postInit.GlobalItems.*
-
-// Removing unwanted items / recipes
-mods.jei.ingredient.removeAndHide(vanillaCraftingTable)
-crafting.removeByOutput(craftingStation)
-crafting.removeByOutput(toolStation)
-crafting.remove('tconstruct:common/flint')
+import mods.tconstruct.melting
+import mods.advancedrocketry.precisionassembler
 
 // 1 gravel = 1 flint
+crafting.remove('tconstruct:common/flint')
 crafting.addShapeless('gravel_to_flint', flint, [gravel])
 
 // Replacing the crafting station recipe to take the role of the vanilla crafting table
+mods.jei.ingredient.removeAndHide(vanillaCraftingTable)
+crafting.removeByOutput(craftingStation)
 crafting.replaceShapeless('simple_crafting_station', craftingStation, [flint, logWood])
 
 // Tool station 2x2 crafting recipe
+crafting.removeByOutput(toolStation)
 crafting.replaceShapeless('simple_tool_station', toolStation, [flint, pattern, plankWood])
 
 // Fix rod melting recipes
-for(item in stickMetal){
+for(item in stickMetalBugged){
     mods.tconstruct.melting.removeByInput(item)
 }
 mods.tconstruct.melting.add(stickIron, fluid('iron') * 72, 514)
@@ -36,3 +36,14 @@ crafting.addShaped(item('minecraft:stick') * 16, [[logWood],[logWood]])
 // Thermal-style Gear recipe for titanium gears
 crafting.removeByOutput(item('libvulpes:productgear', 7))
 crafting.addShaped(item('libvulpes:productgear', 7), [[null, ingotTitanium, null], [ingotTitanium, null, ingotTitanium], [null, ingotTitanium, null]])
+
+// Control Circuit Base recipe
+crafting.removeByOutput(item('jsg:circuit_control_base'))
+mods.advancedrocketry.precisionassembler.recipeBuilder()
+    .input(controlBoard, crystalRed * 2)
+    .fluidInput(blendWhite * 144)
+    .output(item('jsg:circuit_control_base'))
+    .fluidOutput(blendBlack * 72)
+    .power(100)
+    .time(400)
+    .register()
