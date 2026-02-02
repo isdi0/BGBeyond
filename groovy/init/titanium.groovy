9dde6693afc89fec357ee91f28411a67f73e8a17
Eridani System <osf319@gmail.com>
2026-02-02 15:01:38 -0500

    added titanium as a tconstruct material, restructured redundantItems

---
+++ groovy/init/titanium.groovy
@@ -0,0 +1,45 @@
+import slimeknights.mantle.util.RecipeMatchRegistry
+import slimeknights.tconstruct.library.TinkerRegistry
+import slimeknights.tconstruct.library.materials.*
+import slimeknights.tconstruct.library.MaterialIntegration
+
+// Define new material
+def material = new Material('titanium', 0xccc8fa)
+material.setCraftable(false)
+material.setCastable(true)
+material.setVisible()
+
+// Add to registry
+TinkerRegistry.addMaterial(material)
+
+// Add items
+material.addCommonItems('Titanium')
+material.addItem('dustTitanium')
+material.addItem('plateTitanium')
+material.addItem('sheetTitanium')
+material.addItem('gearTitanium', 1, 576)
+material.addItem('coilTitanium', 1, 1152)
+
+// Set the stats
+// head
+material.addStats(new HeadMaterialStats(600, 8.0f, 5.0f, 3))
+
+// handle
+material.addStats(new HandleMaterialStats(0.95f, 200))
+
+// extra
+material.addStats(new ExtraMaterialStats(100))
+
+// Add traits
+material.addTrait(TinkerRegistry.getTrait("lightweight"), "head")
+material.addTrait(TinkerRegistry.getTrait("lightweight"), "handle")
+material.addTrait(TinkerRegistry.getTrait("established"), "extra")
+
+// Integrate the material
+def integration = new MaterialIntegration(
+    material,
+    fluid('titanium').getFluid(),
+    "Titanium"
+)
+
+integration.integrate()
--- groovy/postInit/redundantItems.groovy
+++ groovy/postInit/redundantItems.groovy
@@ -2,5 +2,6 @@ import static postInit.GlobalItems.*
 import mods.jei.ingredient
 import mods.thermal.crucible
 
-mods.thermal.crucible.removeByOutput(fluid('titanium_molten'))
 mods.jei.ingredient.removeAndHide(fluid('titanium_molten'))
+
+mods.thermal.crucible.removeByOutput(fluid('titanium_molten'))
