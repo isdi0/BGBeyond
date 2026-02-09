7fbc6fd2fc2776814f463e7d107c5e029a50aba6
Eridani System <osf319@gmail.com>
2026-02-08 20:22:08 -0500

    added more stuff to redundantItems

--- groovy/postInit/redundantItems.groovy
+++ groovy/postInit/redundantItems.groovy
@@ -1,6 +1,9 @@
 import static postInit.GlobalItems.*
 import mods.jei.ingredient
 import mods.thermal.crucible
+import mods.ticon.table
+import mods.ticon.basin
+import mods.ticon.melting
 
 def redundantIngredients = [
     fluid('titanium_molten'),
@@ -21,20 +24,64 @@ def redundantIngredients = [
     item('rftools:notick_solid_shield_block_opaque'),
     item('rftools:camo_shield_block_opaque'),
     item('rftools:notick_camo_shield_block_opaque'),
-    item('doubleslabs:vertical_slab').withNbt(['item': ['id': 'blockcraftery':'editable_slab', 'Count': 1 'as' 'byte', 'Damage': 0 'as' 'short']]),
-    item('doubleslabs:vertical_slab').withNbt(['item': ['id': 'blockcraftery':'editable_slab_reinforced', 'Count': 1 'as' 'byte', 'Damage': 0 'as' 'short']]),
-    item('doubleslabs:vertical_slab').withNbt(['item': []]),
     item('omlib:multi_tool'),
-    item('zerocore:debugtool')
-    item('libvulpes:productingot', 3),
+    item('zerocore:debugtool'),
+    item('libvulpes:productingot', 3), // libvulpes metadata 3 = Silicon
     item('libvulpes:productdust', 3),
-    item('libvulpes:productnugget', 3)
+    item('libvulpes:productnugget', 3),
+    item('tconstruct:common_ingots'), // Copper, tin, aluminum, bronze, steel already covered by thermal.
+    item('tconstruct:common_ingots', 1),
+    item('tconstruct:common_ingots', 2),
+    item('tconstruct:common_ingots', 3),
+    item('tconstruct:common_ingots', 4),
+    item('tconstruct:common_nuggets'),
+    item('tconstruct:common_nuggets', 1),
+    item('tconstruct:common_nuggets', 2),
+    item('tconstruct:common_nuggets', 3),
+    item('tconstruct:common_nuggets', 4),
+    item('tconstruct:common_metal'),
+    item('tconstruct:common_metal', 1),
+    item('tconstruct:common_metal', 2),
+    item('tconstruct:common_metal', 3),
+    item('tconstruct:common_metal', 4),
+    item('appliedenergistics2:material', 51), // Gold dust already covered by thermal
+    item('appliedenergistics2:material', 49), // Iron dust already covered by thermal
+    item('thermalfoundation:material', 22) // Wooden gear covered by AE2
 ]
+
+def commonItems = [
+    item('tconstruct:common_ingots'),
+    item('tconstruct:common_ingots', 1),
+    item('tconstruct:common_ingots', 2),
+    item('tconstruct:common_ingots', 3),
+    item('tconstruct:common_ingots', 4),
+    item('tconstruct:common_nuggets'),
+    item('tconstruct:common_nuggets', 1),
+    item('tconstruct:common_nuggets', 2),
+    item('tconstruct:common_nuggets', 3),
+    item('tconstruct:common_nuggets', 4)
+]
+def commonMetals = [
+    item('tconstruct:common_metal'),
+    item('tconstruct:common_metal', 1),
+    item('tconstruct:common_metal', 2),
+    item('tconstruct:common_metal', 3),
+    item('tconstruct:common_metal', 4)
+]
+
+for (entry in commonItems) {
+    mods.ticon.table.removeByOutput(entry)
+    mods.ticon.melting.removeByInput(entry)
+}
+for (entry in commonMetals) {
+    mods.ticon.basin.removeByOutput(entry)
+    mods.ticon.melting.removeByInput(entry)
+}
 
 crafting.removeByOutput(item('atum:scarab'))
 
 mods.thermal.crucible.removeByOutput(fluid('titanium_molten'))
 
-for (ingredient in redundantIngredients) {
-    mods.jei.ingredient.removeAndHide(ingredient)
+for (item in redundantIngredients) {
+    mods.jei.ingredient.removeAndHide(item)
 }
