9937c4e469fc8603c27d490379fc81646ddf7d29
Eridani System <osf319@gmail.com>
2026-02-13 17:12:53 -0500

    finished changing gate recipe

--- groovy/postInit/jsgGates.groovy
+++ groovy/postInit/jsgGates.groovy
@@ -62,7 +62,7 @@ def uniChevron = mods.advancedrocketry.precisionassembler.recipeBuilder()
 // inputs
 uniChevron.input(item('jsg:schematic_universe'))
 uniChevron.input(item('jsg:fragment_stargate_universe') * 2)
-uniChevron.input(item('appliedenergistics2:material', 11))
+uniChevron.input(item('thermalfoundation:material', 1))
 uniChevron.input(item('minecraft:glowstone_dust') * 4)
 uniChevron.input(item('minecraft:obsidian'))
 uniChevron.input(item('thermalfoundation:material', 320))
@@ -77,66 +77,178 @@ uniChevron.time(600)
 
 uniChevron.register()
 
-/*
+// Dialer - Grid
+crafting.removeByOutput(item('jsg:universe_dialer'))
+crafting.addShaped('uni_dialer', item('jsg:universe_dialer'), 
+	[
+		[item('minecraft:redstone'), item('advancedrocketry:misc'), item('minecraft:redstone')],
+		[item('jsg:naquadah_alloy'), item('advancedrocketry:ic', 3), item('jsg:naquadah_alloy')],
+		[item('jsg:naquadah_alloy'), item('minecraft:redstone'), item('jsg:naquadah_alloy')]
+	]
+)
+
+// Dialer - Assembler
+def uniDialer = mods.advancedrocketry.precisionassembler.recipeBuilder()
+
+// inputs
+uniDialer.input(item('minecraft:redstone') * 2)
+uniDialer.input(item('jsg:naquadah_alloy') * 2)
+uniDialer.input(item('advancedrocketry:ic', 3))
+uniDialer.input(item('advancedrocketry:misc'))
+
+// output
+uniDialer.output(item('jsg:universe_dialer'))
+
+// power & time
+uniDialer.power(250)
+uniDialer.time(300)
+
+uniDialer.register()
+
+
+
 // MILKY WAY
 // Base
 def mwBase = mods.advancedrocketry.precisionassembler.recipeBuilder()
 
 // inputs
-uniBase.input(item('jsg:schematic_universe'))
-uniBase.input(item('jsg:fragment_stargate_universe') * 2)
-uniBase.input(item('minecraft:diamond'))
-uniBase.input(item('appliedenergistics2:material', 10) * 2)
-uniBase.input(item('appliedenergistics2:material', 3) * 4)
-uniBase.input(item('thermalfoundation:material', 320) * 2)
+mwBase.input(item('jsg:schematic_milkyway'))
+mwBase.input(item('jsg:fragment_stargate_milkyway') * 2)
+mwBase.input(item('appliedenergistics2:material', 24))
+mwBase.input(item('appliedenergistics2:material', 16) * 2)
+mwBase.input(item('appliedenergistics2:material', 3) * 4)
+mwBase.input(item('thermalfoundation:material', 352) * 2)
 
 // outputs
-uniBase.output(item('jsg:stargate_universe_base_block'))
-uniBase.output(item('jsg:schematic_universe'))
+mwBase.output(item('jsg:stargate_milkyway_base_block'))
+mwBase.output(item('jsg:schematic_milkyway'))
 
 // power & time
-uniBase.power(250)
-uniBase.time(1200)
+mwBase.power(250)
+mwBase.time(1500)
 
-uniBase.register()
+mwBase.register()
 
 // Ring
 def mwRing = mods.advancedrocketry.precisionassembler.recipeBuilder()
 
 // inputs
-uniRing.input(item('jsg:schematic_universe'))
-uniRing.input(item('jsg:fragment_stargate_universe') * 3)
-uniRing.input(item('thermalfoundation:material', 770) * 6)
-uniRing.input(item('thermalfoundation:material', 320)  * 2)
+mwRing.input(item('jsg:schematic_milkyway'))
+mwRing.input(item('jsg:fragment_stargate_milkyway') * 3)
+mwRing.input(item('thermalfoundation:material', 513) * 2)
+mwRing.input(item('thermalfoundation:material', 352)  * 2)
 
 // outputs
-uniRing.output(item('jsg:stargate_universe_member_block', 6) * 2)
-uniRing.output(item('jsg:schematic_universe'))
+mwRing.output(item('jsg:stargate_milkyway_member_block', 6) * 2)
+mwRing.output(item('jsg:schematic_milkyway'))
 
 // power & time
-uniRing.power(250)
-uniRing.time(600)
+mwRing.power(250)
+mwRing.time(750)
 
-uniRing.register()
+mwRing.register()
 
 // Chevron
 def mwChevron = mods.advancedrocketry.precisionassembler.recipeBuilder()
 
 // inputs
-uniChevron.input(item('jsg:schematic_universe'))
-uniChevron.input(item('jsg:fragment_stargate_universe') * 2)
-uniChevron.input(item('appliedenergistics2:material', 11))
-uniChevron.input(item('minecraft:glowstone_dust') * 4)
-uniChevron.input(item('minecraft:obsidian'))
-uniChevron.input(item('thermalfoundation:material', 320))
+mwChevron.input(item('jsg:schematic_milkyway'))
+mwChevron.input(item('jsg:fragment_stargate_milkyway') * 2)
+mwChevron.input(item('appliedenergistics2:material', 18))
+mwChevron.input(ore('projredIllumar') * 2)
+mwChevron.input(ore('blockGlassHardened'))
+mwChevron.input(item('thermalfoundation:material', 352))
 
 // outputs
-uniChevron.output(item('jsg:stargate_universe_member_block', 14))
-uniChevron.output(item('jsg:schematic_universe'))
+mwChevron.output(item('jsg:stargate_milkyway_member_block', 14))
+mwChevron.output(item('jsg:schematic_milkyway'))
 
 // power & time
-uniChevron.power(250)
-uniChevron.time(600)
+mwChevron.power(250)
+mwChevron.time(750)
 
-uniChevron.register()
-*/
+mwChevron.register()
+
+// Dialer
+def mwDialer = mods.advancedrocketry.precisionassembler.recipeBuilder()
+
+// inputs
+mwDialer.input(item('jsg:schematic_milkyway'))
+mwDialer.input(item('appliedenergistics2:material', 22))
+mwDialer.input(item('minecraft:redstone') * 8)
+mwDialer.input(item('jsg:naquadah_alloy') * 2)
+mwDialer.input(item('thermalfoundation:material', 352) * 4)
+
+// outputs
+mwDialer.output(item('jsg:dhd_block'))
+mwDialer.output(item('jsg:schematic_milkyway'))
+
+// power & time
+mwDialer.power(250)
+mwDialer.time(600)
+
+mwDialer.register()
+
+
+/*
+// PEGASUS
+// Base
+def pegBase = mods.advancedrocketry.precisionassembler.recipeBuilder()
+
+// inputs
+pegBase.input(item('jsg:schematic_pegasus'))
+pegBase.input(item('jsg:fragment_stargate_pegasus') * 2)
+pegBase.input(item('appliedenergistics2:material', 24))
+pegBase.input(item('appliedenergistics2:material', 16) * 2)
+pegBase.input(item('appliedenergistics2:material', 3) * 4)
+pegBase.input(item('thermalfoundation:material', 352) * 2)
+
+// outputs
+pegBase.output(item('jsg:stargate_pegasus_base_block'))
+pegBase.output(item('jsg:schematic_pegasus'))
+
+// power & time
+pegBase.power(250)
+pegBase.time(1200)
+
+pegBase.register()
+
+// Ring
+def pegRing = mods.advancedrocketry.precisionassembler.recipeBuilder()
+
+// inputs
+pegRing.input(item('jsg:schematic_pegasus'))
+pegRing.input(item('jsg:fragment_stargate_pegasus') * 3)
+pegRing.input(item('thermalfoundation:material', 513) * 2)
+pegRing.input(item('thermalfoundation:material', 352)  * 2)
+
+// outputs
+pegRing.output(item('jsg:stargate_pegasus_member_block', 6) * 2)
+pegRing.output(item('jsg:schematic_pegasus'))
+
+// power & time
+pegRing.power(250)
+pegRing.time(600)
+
+pegRing.register()
+
+// Chevron
+def pegChevron = mods.advancedrocketry.precisionassembler.recipeBuilder()
+
+// inputs
+pegChevron.input(item('jsg:schematic_pegasus'))
+pegChevron.input(item('jsg:fragment_stargate_pegasus') * 2)
+pegChevron.input(item('appliedenergistics2:material', 18))
+pegChevron.input(ore('projredIllumar') * 2)
+pegChevron.input(ore('blockGlassHardened'))
+pegChevron.input(item('thermalfoundation:material', 352))
+
+// outputs
+pegChevron.output(item('jsg:stargate_pegasus_member_block', 14))
+pegChevron.output(item('jsg:schematic_pegasus'))
+
+// power & time
+pegChevron.power(250)
+pegChevron.time(600)
+
+pegChevron.register() */
