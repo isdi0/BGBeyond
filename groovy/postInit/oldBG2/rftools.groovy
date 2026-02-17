fc92faf47c4da37f0b5b63008f492ecf9f9f3d27
Eridani System <osf319@gmail.com>
2026-02-16 21:59:36 -0500

    rewrote rftools.groovy

--- groovy/postInit/oldBG2/rftools.groovy
+++ groovy/postInit/oldBG2/rftools.groovy
@@ -7,9 +7,49 @@ crafting.removeByOutput(item('rftools:matter_receiver'))
 crafting.removeByOutput(item('rftools:matter_transmitter'))
 crafting.removeByOutput(item('rftools:machine_frame'))
 //Add crafting
-crafting.addShaped(item('rftools:elevator'), [[item('minecraft:redstone'), item('minecraft:piston'), item('minecraft:redstone')],[item('minecraft:redstone'), item('thermalexpansion:frame'), item('minecraft:redstone')], [item('minecraft:redstone'), item('minecraft:redstone_torch'), item('minecraft:redstone')]])
-crafting.addShaped(item('rftools:screen_controller'), [[item('minecraft:redstone'), item('minecraft:ender_pearl'), item('minecraft:redstone')],[item('minecraft:glass'), item('thermalexpansion:frame'), item('minecraft:glass')], [item('minecraft:redstone'), item('minecraft:glass'), item('minecraft:redstone')]])
-crafting.addShaped(item('rftools:destination_analyzer'), [[item('jsg:naquadah_alloy_raw'), item('minecraft:redstone'), item('jsg:naquadah_alloy_raw')],[item('minecraft:redstone'), item('thermalexpansion:frame'), item('minecraft:redstone')], [item('jsg:naquadah_alloy_raw'), item('minecraft:redstone'), item('jsg:naquadah_alloy_raw')]])
-crafting.addShaped(item('rftools:dialing_device'), [[item('minecraft:redstone'), item('minecraft:redstone'), item('minecraft:redstone')],[item('minecraft:redstone_torch'), item('thermalexpansion:frame'), item('minecraft:redstone_torch')], [item('minecraft:redstone'), item('minecraft:redstone'), item('minecraft:redstone')]])
-crafting.addShaped(item('rftools:matter_receiver'), [[item('minecraft:iron_ingot'), item('minecraft:iron_ingot'), item('minecraft:iron_ingot')],[item('minecraft:redstone'), item('thermalexpansion:frame'), item('minecraft:redstone')], [item('jsg:naquadah_alloy_raw'), item('jsg:naquadah_alloy_raw'), item('jsg:naquadah_alloy_raw')]])
-crafting.addShaped(item('rftools:matter_transmitter'), [[item('jsg:naquadah_alloy_raw'), item('jsg:naquadah_alloy_raw'), item('jsg:naquadah_alloy_raw')],[item('minecraft:redstone'), item('thermalexpansion:frame'), item('minecraft:redstone')], [item('minecraft:iron_ingot'), item('minecraft:iron_ingot'), item('minecraft:iron_ingot')]]);
+rs = item('minecraft:redstone')
+rt = item('minecraft:redstone_torch')
+pt = item('minecraft:piston')
+bi = item('jsg:naquadah_alloy_raw')
+fi = item('minecraft:iron_ingot')
+bG = item('minecraft:glass')
+pE = item('minecraft:ender_pearl')
+fm = item('rftools:machine_frame')
+ii = item('thermalfoundation:material', 162)
+bg = item('thermalfoundation:material', 291)
+hG = item('thermalfoundation:glass', 3)
+crafting.addShaped(item('rftools:elevator'), [
+    [rs, pt, rs],
+    [rs, fm, rs], 
+    [rs, rt, rs]
+])
+crafting.addShaped(item('rftools:screen_controller'), [
+    [rs, pE, rs],
+    [bG, fm, bG],
+    [rs, bG, rs]
+])
+crafting.addShaped(item('rftools:destination_analyzer'), [
+    [bi, rs, bi],
+    [rs, fm, rs],
+    [bi, rs, bi]
+])
+crafting.addShaped(item('rftools:dialing_device'), [
+    [rs, rs, rs],
+    [item('minecraft:redstone_torch'), fm, item('minecraft:redstone_torch')],
+    [rs, rs, rs]
+])
+crafting.addShaped(item('rftools:matter_receiver'), [
+    [fi, fi, fi],
+    [rs, fm, rs],
+    [bi, bi, bi]
+])
+crafting.addShaped(item('rftools:matter_transmitter'), [
+    [bi, bi, bi],
+    [rs, fm, rs], 
+    [fi, fi, fi]
+])
+crafting.addShaped(fm, [
+    [ii, hG, ii],
+    [hG, bg, hG],
+    [ii, hG, ii]
+])
