1ac3b28063bdf191730912cc75c5f1535434b68d
Eridani System <osf319@gmail.com>
2026-02-20 16:56:49 -0500

    restructured redundantItems for clarity, added comments where necessary

--- groovy/postInit/redundantItems.groovy
+++ groovy/postInit/redundantItems.groovy
@@ -7,51 +7,7 @@ import mods.ticon.melting
 import slimeknights.tconstruct.library.smeltery.MeltingRecipe
 import slimeknights.tconstruct.library.smeltery.CastingRecipe
 
-def redundantIngredients = [
-    fluid('titanium_molten'),
-    fluid('fuelcolumn'),
-    item('mcjtylib_ng:multipart'),
-    item('omlib:fake_sword'),
-    item('omlib:network_cable'),
-    item('omlib:debug_tool'),
-    item('rftools:invisible_shield_block'),
-    item('rftools:notick_invisible_shield_block'),
-    item('rftools:invisible_shield_block_opaque'),
-    item('rftools:notick_invisible_shield_block_opaque'),
-    item('rftools:solid_shield_block'),
-    item('rftools:notick_solid_shield_block'),
-    item('rftools:camo_shield_block'),
-    item('rftools:notick_camo_shield_block'),
-    item('rftools:solid_shield_block_opaque'),
-    item('rftools:notick_solid_shield_block_opaque'),
-    item('rftools:camo_shield_block_opaque'),
-    item('rftools:notick_camo_shield_block_opaque'),
-    item('omlib:multi_tool'),
-    item('zerocore:debugtool'),
-    item('libvulpes:productingot', 3), // libvulpes metadata 3 = Silicon
-    item('libvulpes:productdust', 3),
-    item('libvulpes:productnugget', 3),
-    item('tconstruct:common_ingots'), // Copper, tin, aluminum, bronze, steel already covered by thermal.
-    item('tconstruct:common_ingots', 1),
-    item('tconstruct:common_ingots', 2),
-    item('tconstruct:common_ingots', 3),
-    item('tconstruct:common_ingots', 4),
-    item('tconstruct:common_nuggets'),
-    item('tconstruct:common_nuggets', 1),
-    item('tconstruct:common_nuggets', 2),
-    item('tconstruct:common_nuggets', 3),
-    item('tconstruct:common_nuggets', 4),
-    item('tconstruct:common_metal'),
-    item('tconstruct:common_metal', 1),
-    item('tconstruct:common_metal', 2),
-    item('tconstruct:common_metal', 3),
-    item('tconstruct:common_metal', 4),
-    item('appliedenergistics2:material', 51), // Gold dust already covered by thermal
-    item('appliedenergistics2:material', 49), // Iron dust already covered by thermal
-    item('thermalfoundation:material', 22), // Wooden gear covered by AE2
-    item('bigreactors:oreyellorite')
-]
-
+// copper, tin, aluminum, bronze, steel already covered by thermal
 def commonItems = [
     item('tconstruct:common_ingots'),
     item('tconstruct:common_ingots', 1),
@@ -72,6 +28,76 @@ def commonMetals = [
     item('tconstruct:common_metal', 4)
 ]
 
+// yellorium, blutonium, ludicrite are being replaced
+def nuclearIngots = [
+    item('bigreactors:ingotyellorium'),
+    item('bigreactors:ingotcyanite'),
+    item('bigreactors:ingotblutonium')
+]
+def nuclearDusts = [
+    item('bigreactors:dustyellorium'),
+    item('bigreactors:dustcyanite'),
+    item('bigreactors:dustblutonium'),
+    item('bigreactors:dustludicrite')
+]
+def nuclearBlocks = [
+    item('bigreactors:blockyellorium'),
+    item('bigreactors:blockcyanite'),
+    item('bigreactors:blockblutonium')
+]
+
+
+// shield blocks are unobtainable as items, and not even craftable, so they're just cluttering up JEI
+def rfShieldBlocks = [
+    item('rftools:invisible_shield_block'),
+    item('rftools:notick_invisible_shield_block'),
+    item('rftools:invisible_shield_block_opaque'),
+    item('rftools:notick_invisible_shield_block_opaque'),
+    item('rftools:solid_shield_block'),
+    item('rftools:notick_solid_shield_block'),
+    item('rftools:camo_shield_block'),
+    item('rftools:notick_camo_shield_block'),
+    item('rftools:solid_shield_block_opaque'),
+    item('rftools:notick_solid_shield_block_opaque'),
+    item('rftools:camo_shield_block_opaque'),
+    item('rftools:notick_camo_shield_block_opaque')
+]
+
+def redundantIngredients = [
+    fluid('titanium_molten'),
+    fluid('fuelcolumn'),
+    fluid('yellorium')
+    item('mcjtylib_ng:multipart'),
+    item('omlib:fake_sword'),
+    item('omlib:network_cable'),
+    item('omlib:debug_tool'),
+    item('omlib:multi_tool'),
+    item('zerocore:debugtool'),
+    item('libvulpes:productingot', 3), // libvulpes metadata 3 = Silicon
+    item('libvulpes:productdust', 3),
+    item('libvulpes:productnugget', 3),
+    item('appliedenergistics2:material', 51), // Gold dust already covered by thermal
+    item('appliedenergistics2:material', 49), // Iron dust already covered by thermal
+    item('thermalfoundation:material', 22), // Wooden gear covered by AE2
+    item('bigreactors:oreyellorite')
+    rfShieldBlocks,
+    commonItems, 
+    commonMetals,
+    nuclearIngots,
+    nuclearDusts, 
+    nuclearBlocks,
+]
+redundantIngredients.flatten()
+
+crafting.removeByOutput(item('atum:scarab'))
+
+mods.thermal.crucible.removeByOutput(fluid('titanium_molten'))
+
+// hide from JEI
+for (item in redundantIngredients) {
+    mods.jei.ingredient.removeAndHide(item)
+}
+
 for (entry in commonItems) {
     mods.ticon.table.removeByOutput(entry)
     mods.ticon.melting.removeByInput(entry)
@@ -82,7 +108,7 @@ for (entry in commonMetals) {
     mods.ticon.melting.removeByInput(entry)
 }
 
-// Restore melting recipes for thermal
+// Melting recipes for thermal
 // copper
 mods.ticon.melting.add(item('thermalfoundation:material', 128), fluid('copper') * 144, MeltingRecipe.calcTemperature(fluid('copper').getFluid().getTemperature(), 144))
 mods.ticon.melting.add(item('thermalfoundation:material', 192), fluid('copper') * 16, MeltingRecipe.calcTemperature(fluid('copper').getFluid().getTemperature(), 16))
@@ -104,7 +130,7 @@ mods.ticon.melting.add(item('thermalfoundation:material', 160), fluid('steel') *
 mods.ticon.melting.add(item('thermalfoundation:material', 224), fluid('steel') * 16, MeltingRecipe.calcTemperature(fluid('steel').getFluid().getTemperature(), 16))
 mods.ticon.melting.add(item('thermalfoundation:storage_alloy'), fluid('steel') * 1296, MeltingRecipe.calcTemperature(fluid('steel').getFluid().getTemperature(), 1296))
 
-// Restore casting recipes for thermal
+// Casting recipes for thermal
 // copper
 mods.ticon.table.recipeBuilder()
     .fluidInput(fluid('copper') * 144)
@@ -196,45 +222,17 @@ mods.ticon.basin.recipeBuilder()
     .cast(null)
     .coolingTime(CastingRecipe.calcCooldownTime(fluid('steel').getFluid(), 1296))
 
-crafting.removeByOutput(item('atum:scarab'))
-
-mods.thermal.crucible.removeByOutput(fluid('titanium_molten'))
-
-for (item in redundantIngredients) {
-    mods.jei.ingredient.removeAndHide(item)
-}
-
-def nuclearIngots = [
-    item('bigreactors:ingotyellorium'),
-    item('bigreactors:ingotcyanite'),
-    item('bigreactors:ingotblutonium')
-]
-def nuclearDusts = [
-    item('bigreactors:dustyellorium'),
-    item('bigreactors:dustcyanite'),
-    item('bigreactors:dustblutonium'),
-    item('bigreactors:dustludicrite')
-]
-def nuclearBlocks = [
-    item('bigreactors:blockyellorium'),
-    item('bigreactors:blockcyanite'),
-    item('bigreactors:blockblutonium')
-]
-
 for(entry in nuclearIngots) {
     crafting.removeByOutput(entry)
     Furnace.removeByOutput(entry)
     mods.thermal.Furnace.removeByOutput(entry)
     mods.thermal.Smelter.removeByOutput(entry)
-    mods.jei.ingredient.removeAndHide(entry)
 }
 for(entry in nuclearDusts) {
     mods.thermal.Pulverizer.removeByOutput(entry)
-    mods.jei.ingredient.removeAndHide(entry)
 }
 for(entry in nuclearBlocks) {
     crafting.removeByOutput(entry)
     mods.thermal.Factorizer.removeByOutput(entry)
     mods.thermal.Factorizer.removeByInput(entry)
-    mods.jei.ingredient.removeAndHide(entry)
 }
