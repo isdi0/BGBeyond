d358652b7e56ea7c38e843d649056fd93cf46869
Eridani System <osf319@gmail.com>
2026-02-22 02:05:22 -0500

    Fixed extreme reactors solids not registering properly

--- groovy/postInit/extremereactors.groovy
+++ groovy/postInit/extremereactors.groovy
@@ -5,8 +5,8 @@ import erogenousbeef.bigreactors.api.registry.Reactants
 Reactants.registerReactant('pyrotheum_fuel', 0, 0xf6e4bd)
 Reactants.registerReactant('pyrotheum_waste', 1, 0x262626)
 
-// Add oredict items to reactants
+// Add items to reactants
 Reactants.registerSolid(item('thermalfoundation:material', 1024), 'pyrotheum_fuel', 500)
-Reactants.registerSolid(item('bgbeyond:pyrotheum_slag'), 'pyrotheum_waste', 500)
+Reactants.registerSolid('crystalPyrotheumSlag', 'pyrotheum_waste', 500)
 
 ReactorConversions.register('pyrotheum_fuel', 'pyrotheum_waste', 1.4f, 1f)
